e7d023b2d6f66b1416e6ecc1a2dd0def6f3fdc01
d7laungani <d7laungani@hotmail.com>
2016-06-19 03:38:33 -0400

    Custom cell for location settings

---
+++ StandUp-Production/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  StandUp-Production
+//
+//  Created by Devesh Laungani on 6/19/16.
+//  Copyright © 2016 Devesh Laungani. All rights reserved.
+//
+
+import UIKit
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var switchModule: UISwitch!
+    @IBOutlet weak var myCellLabel: UILabel!
+}
