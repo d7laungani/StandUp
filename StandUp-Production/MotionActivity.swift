4df1f722578972a30b26c4b3e17af750a14149e0
d7laungani <d7laungani@hotmail.com>
2016-06-21 02:28:28 -0400

    making function modular

--- StandUp-Production/MotionActivity.swift
+++ StandUp-Production/MotionActivity.swift
@@ -14,41 +14,43 @@ class MotionActivity {
     
     
     var historyProcessor = HistoryProcessor()
-    
-    class func getHistoricalData(activityManager: CMMotionActivityManager, getHistCompletionHandler : (activities: [CMMotionActivity] , error: NSError? ) -> Void ){
+    static let cal: NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+    class func getHistoricalData(activityManager: CMMotionActivityManager,
         
-        let cal: NSCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)!
+        fromDate : NSDate = HistoryProcessor.findMidnightOfDay(NSDate(timeIntervalSinceNow: -86400 * 7 ) ),
+        tillDate: NSDate = cal.dateBySettingHour(0, minute: 0, second: 0, ofDate: NSDate(), options: NSCalendarOptions())!,
+        getHistCompletionHandler : (activities: [CMMotionActivity] , error: NSError? ) -> Void ){
         
-       
-        
-        let fromDate = HistoryProcessor.findMidnightOfDay(NSDate(timeIntervalSinceNow: -86400 * 7 ) )
-      let tillDate: NSDate = cal.dateBySettingHour(0, minute: 0, second: 0, ofDate: NSDate(), options: NSCalendarOptions())!
         var activities = [CMMotionActivity]()
         
-        //print("from date is \(fromDate)")
-        //print("till date is \(tillDate)")
+        print("from date is \(fromDate)")
+        print("till date is \(tillDate)")
         
         activityManager.queryActivityStartingFromDate(fromDate, toDate: tillDate, toQueue: NSOperationQueue.mainQueue()) { (data, error) -> Void in
+            
+            
             if (error == nil) {
                 
-                if let info = data! as? [CMMotionActivity] {
-                    
-                    activities = info
-                   
-                }
+                activities = data! as [CMMotionActivity]
                 
                 
-                
+
             }
             
             getHistCompletionHandler(activities: activities, error: error )
         }
         
         
-    
-       
+        
+        
     }
+    
+    enum activityError: ErrorType {
         
+        case NoActivities
         
-        
+    }
+    
+    
+    
 }
