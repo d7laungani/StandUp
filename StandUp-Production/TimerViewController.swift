3205036c3acc05ff9881e4ca7b86896d22af8dd7
d7laungani <d7laungani@hotmail.com>
2016-06-22 01:57:37 -0400

    setup button iboutlet collection and border info for buttons

--- StandUp-Production/TimerViewController.swift
+++ StandUp-Production/TimerViewController.swift
@@ -7,7 +7,60 @@
 //
 
 import UIKit
+import QuartzCore
 
 class TimerViewController: UIViewController {
+    
+    
+    
+    
+    let step: Float = 5
+    @IBOutlet var daysButtons: [UIButton]!
+    
+    @IBOutlet weak var intervalLabel: UILabel!
+    
+    
+    @IBAction func timeIntervalSlider(sender: UISlider) {
+        
+        
+        
+        let roundedValue = round(sender.value / step) * step
+        sender.value = roundedValue
+        
+        let s: Int = 00
+        let m: Int = Int(roundedValue)
+        
+        let formattedDuration = String(format: "%0d:%02d", m, s)
+        
+        intervalLabel.text = formattedDuration
+    }
+    
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUIElements()
+        
+        
+    }
+    
+    func setupUIElements () {
+        
+        
+        for button in daysButtons {
+           
+            button.layer.cornerRadius = 5
+            button.layer.borderWidth = 1.1
+            button.layer.borderColor = UIColor.blueColor().CGColor
+            
+            
+        }
+        
+        
+    }
 
+    
+    
 }
