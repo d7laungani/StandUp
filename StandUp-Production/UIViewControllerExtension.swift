00131179721312d8669cd3918cce40e684e5ddba
d7laungani <d7laungani@hotmail.com>
2016-06-22 22:07:22 -0400

    extension for dismissing keyboard when tapped out of keyboard area

---
+++ StandUp-Production/UIViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtension.swift
+//  StandUp-Production
+//
+//  Created by Devesh Laungani on 6/22/16.
+//  Copyright © 2016 Devesh Laungani. All rights reserved.
+//
+
+import Foundation
