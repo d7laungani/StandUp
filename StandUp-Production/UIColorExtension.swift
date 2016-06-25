3e97d26d00f351a3efc0fb67ec558452bede0f40
d7laungani <d7laungani@hotmail.com>
2016-06-25 16:31:10 -0400

    color extensions for hex to UIColor

---
+++ StandUp-Production/UIColorExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColorExtension.swift
+//  StandUp-Production
+//
+//  Created by Devesh Laungani on 6/25/16.
+//  Copyright © 2016 Devesh Laungani. All rights reserved.
+//
+
+import Foundation
