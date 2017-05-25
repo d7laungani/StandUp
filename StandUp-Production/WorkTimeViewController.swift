e2f88a9a66d17a476838c78eb968f10cb0535e36
d7laungani <d7laungani@hotmail.com>
2017-05-25 13:06:24 -0500

    Fixed issue with correctly displaying and saving times across different timezones

--- StandUp-Production/WorkTimeViewController.swift
+++ StandUp-Production/WorkTimeViewController.swift
@@ -9,145 +9,102 @@
 import UIKit
 import SwiftyUserDefaults
 import _10Clock
+import SwiftDate
 
 class WorkTimeViewController: UIViewController, TenClockDelegate {
-    
+
     @IBOutlet weak var startWorkTime: UIDatePicker!
     @IBOutlet weak var endWorkTime: UIDatePicker!
-    
     @IBOutlet var clock: TenClock!
-    
-    
-     var settings = Defaults[.settings]
-    
+
+    var settings = Defaults[.settings]
+
     override func viewDidLoad() {
-        
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         setupUI()
-        
+
         let calendar = Calendar.current
-    
-        
         clock.delegate = self
-            
-        let date9h = calendar.date(bySettingHour: 21, minute: 0, second: 0, of: Date())!
-        let date17h = calendar.date(bySettingHour: 5, minute: 0, second: 0, of: Date())!
-        
-        
-        
-        if (settings?.startTime != date9h) {
-            
-            
-            
-            clock.endDate = (settings?.startTime)!
-            
-            
+
+        var cmp = DateComponents()
+        cmp.timeZone = TimeZone.current
+        cmp.calendar = Calendar.current
+        cmp.calendar?.locale = Locale.current
+        cmp.year = Calendar.current.component(.year, from: Date())
+        cmp.month = Calendar.current.component(.month, from: Date())
+        cmp.day = Calendar.current.component(.day, from: Date())
+        cmp.hour = 9
+        let date9h = try! DateInRegion(components: cmp)
+        cmp.hour = 17
+        let date17h = try! DateInRegion(components: cmp)
+
+        if (settings?.startTime != date9h?.absoluteDate) {
+
+            clock.startDate = (  settings?.startTime)!
+
         } else {
-            clock.startDate = date9h
-            
+
+            clock.startDate = (date9h?.absoluteDate)!
+
         }
-        
-        if (settings?.endTime != date17h) {
-           // print (settings?.endTime.debugDescription)
-            /*
-            var endComponents = DateComponents()
-            
-            endComponents.hour = Calendar.current.component(.hour, from: (clock.startDate))
-            endComponents.minute = Calendar.current.component(.minute, from: (clock.startDate))
-            print (endComponents.hour! - 12)
-            clock.startDate = Calendar.current.date(bySettingHour: abs(endComponents.hour! - 12), minute: endComponents.minute!, second: 0, of:  Calendar.current.startOfDay(for: Date()))!
-            
-            print(clock.startDate.debugDescription)
-           */
-            
-            clock.startDate = (settings?.endTime)!
-            
+
+        if (settings?.endTime != date17h?.absoluteDate) {
+
+            clock.endDate = (  settings?.endTime)!
+
         } else {
-            
-            clock.endDate = date17h
+
+            clock.endDate = (date17h?.absoluteDate)!
         }
-        
-        
-        
-        
+
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
-      
-        settings?.startTime = clock.endDate
-        settings?.endTime = clock.startDate
-        
+
+        var cmp = DateComponents()
+        cmp.timeZone = TimeZone.current
+        cmp.calendar = Calendar.current
+        cmp.calendar?.locale = Locale.current
+        cmp.year = Calendar.current.component(.year, from: Date())
+        cmp.month = Calendar.current.component(.month, from: Date())
+        cmp.day = Calendar.current.component(.day, from: Date())
+
+        cmp.hour = Calendar.current.component(.hour, from: clock.startDate)
+        cmp.minute = Calendar.current.component(.minute, from: clock.startDate)
+
+        let startDate = try! DateInRegion(components: cmp)
+
+        cmp.hour = Calendar.current.component(.hour, from: clock.endDate)
+        cmp.minute = Calendar.current.component(.minute, from: clock.endDate)
+
+        let endDate = try! DateInRegion(components: cmp)
+
+        settings?.startTime = (startDate?.absoluteDate)!
+        settings?.endTime = (endDate?.absoluteDate)!
         saveSettings()
-        
-        //print("starting and ending times are:")
-        //print(settings?.startTime)
-        //print(settings?.endTime)
-        
-        
+
     }
     func setupUI() {
          self.view.backgroundColor = UIColor(red: CGFloat(31 / 255.0), green: CGFloat(61 / 255.0), blue: CGFloat(91 / 255.0), alpha: CGFloat(1.0))
         clock.numeralsColor = UIColor.white
-       
-        
-        
+
     }
     func saveSettings () {
         Defaults[.settings]? = settings!
         Defaults.synchronize()
-        
+
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-   
 
-    
     //Executed for every touch.
-    func timesUpdated(_ clock:TenClock, startDate:Date,  endDate:Date  ) -> (){
-        
-        /*
-        var startComponents = DateComponents()
-        
-        startComponents.hour = Calendar.current.component(.hour, from: (clock.endDate))
-        startComponents.minute = Calendar.current.component(.minute, from: (clock.endDate))
-        
-        print(startComponents.hour!)
-        print(startComponents.hour! - 12)
-        
-        
-     
-        var endComponents = DateComponents()
-        
-        endComponents.hour = Calendar.current.component(.hour, from: (clock.startDate))
-        endComponents.minute = Calendar.current.component(.minute, from: (clock.startDate))
-        
-        print(endComponents.hour!)
-        print(endComponents.hour! + 12)
-        
-        print (" ")
-     
-        print(clock.endDate)
-        print(clock.startDate)
-        
-        print (" ")
-        */
+    func timesUpdated(_ clock: TenClock, startDate: Date, endDate: Date  ) {
+
     }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
+
 }
-
-
