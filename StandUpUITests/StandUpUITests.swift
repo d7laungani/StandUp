11294da062d23a6f2573ae798a0fea4365096e25
d7laungani <d7laungani@hotmail.com>
2018-04-07 14:11:57 -0500

    Add UI tests

--- StandUpUITests/StandUpUITests.swift
+++ StandUpUITests/StandUpUITests.swift
@@ -14,12 +14,13 @@ class StandUpUITests: XCTestCase {
         super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let app = XCUIApplication()
+        setupSnapshot(app)
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -30,6 +31,22 @@ class StandUpUITests: XCTestCase {
     
     func testExample() {
         // Use recording to get started writing UI tests.
+        
+    
+        snapshot("Notifications")
+        
+        let app = XCUIApplication()
+        app.buttons["WSettings"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Work Times"]/*[[".cells.staticTexts[\"Work Times\"]",".staticTexts[\"Work Times\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        snapshot("Times")
+        
+        app.navigationBars["StandUp_Production.WorkTimeView"].buttons["Done"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.textFields["Live Longer by Standing Up  Now"]/*[[".cells.textFields[\"Live Longer by Standing Up  Now\"]",".textFields[\"Live Longer by Standing Up  Now\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCUIApplication().tables.cells.containing(.staticText, identifier:"Notification Message").children(matching: .textField).element.typeText("This is my custom message")
+        snapshot("Customize")
+       
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
